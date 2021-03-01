b2100f5611fd0501a42fe4d55a6728b25ba484a1
Mathieu Moalic <mathieu.q.moalic@gmail.com>
2021-03-01 12:33:37 +0100

    add basic working layout

--- mx3s/server/views.py
+++ mx3s/server/views.py
@@ -9,10 +9,37 @@ from .models import Simulation
 
 class IndexView(generic.ListView):
     template_name = "server/index.html"
-    context_object_name = "sim_obj"
+    context_object_name = "sims"
+    # model = Simulation  # same as  queryset = Simulation.objects.all()
+    # def get_queryset(self):
+    #     queued = Simulation.objects.all()
+    #     running = Simulation.objects.all()
+    #     finished = Simulation.objects.all()
+    #     return Simulation.objects.all()
+
+    # def get_queue(self):
+    #     return Simulation.objects.
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['now'] = timezone.now()
+    #     return context
 
     def get_queryset(self):
-        return Simulation.objects.all()
+
+        return {
+            "queued": Simulation.objects.filter(is_queued=True),
+            "running": Simulation.objects.filter(is_running=True),
+            "finished": Simulation.objects.filter(is_finished=True),
+        }
+
+
+class Finished(generic.ListView):
+    template_name = "server/finshed.html"
+    context_object_name = "sims"
+
+    def get_queryset(self):
+        return Simulation.objects.filter(is_finished=True)
 
 
 class SimulationCreateView(generic.CreateView):
