75554c656811b1aabb75a1fd1101481e6df3061d
Mathieu Moalic <mathieu.q.moalic@gmail.com>
2021-03-01 10:07:01 +0100

    change base url for server

--- mx3s/server/urls.py
+++ mx3s/server/urls.py
@@ -5,6 +5,8 @@ from . import views
 app_name = "server"
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
+    path("redirect/", views.redirect_view, name="redirect")
+    # path("", views.IndexView.as_view(), name="index"),
     # path("<int:pk>/", views.DetailView.as_view(), name="detail"),
     # path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
     # path("<int:question_id>/vote/", views.vote, name="vote"),
