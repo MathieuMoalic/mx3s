5ebb492021648eeb75786f9a98523beaea1ee7b2
Mathieu Moalic <mathieu.q.moalic@gmail.com>
2021-03-02 21:46:50 +0100

    add basic docker deployement

--- mx3s/__main__.py
+++ mx3s/__main__.py
@@ -1,6 +1,5 @@
 """Main entry point."""
 # pylint: disable=invalid-name
-import mx3s
 
 if __name__ == '__main__':
-  pass
+    pass
--- mx3s/mx3s/settings.py
+++ mx3s/mx3s/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ["DEBUG"]
+DEBUG = int(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*", "127.0.0.1"]
 
 # Application definition
 
@@ -72,13 +72,28 @@ WSGI_APPLICATION = 'mx3s.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3'
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3'
+    "default": {
+        "ENGINE":
+        os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        "NAME":
+        os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
+        "USER":
+        os.environ.get("SQL_USER", "user"),
+        "PASSWORD":
+        os.environ.get("SQL_PASSWORD", "password"),
+        "HOST":
+        os.environ.get("SQL_HOST", "localhost"),
+        "PORT":
+        os.environ.get("SQL_PORT", "5432"),
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
@@ -118,6 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
